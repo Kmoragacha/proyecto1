cac00a0fa997e9dbdee0da31ede5a5c40e792b71
kmoragacha <kmoragacha@gmail.com>
2025-11-19 20:37:17 -0600

    Proyecto1

--- main.cpp
+++ main.cpp
@@ -349,11 +349,4 @@ int main() {
 }                               
 
 
-////////////////----------------------------------///////////////////////////////////////
-//#include <iostream>
 
-///* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-//int main(int argc, char** argv) {
-//	return 0;
-//}
